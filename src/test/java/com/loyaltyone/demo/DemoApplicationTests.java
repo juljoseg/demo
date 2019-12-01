30be4289a11adef710c9189b57edec95b819ca10
julio <38661081+juljoseg@users.noreply.github.com>
2019-11-30 22:58:44 -0500

    Init proyect

---
+++ src/test/java/com/loyaltyone/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyaltyone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
